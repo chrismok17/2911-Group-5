8ec8b66b8977ad12507efe3f11d41e6da5457809
Chris M <cmok97@gmail.com>
2022-05-01 11:34:48 -0700

    Set up app.js (modules and directories) and created home route

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 8080;
+
+// Tell the app to use /CSS directory for CSS files
+app.use("/CSS", express.static(path.join(__dirname, "CSS")));
+
+// Tell app to use /Assets for logo files
+app.use("/Assets", express.static(path.join(__dirname, "Assets")));
+
+// Default route for home page
+app.use("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+// Tell app to listen on port
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
