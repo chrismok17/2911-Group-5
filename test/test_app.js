6c98587746d27fb6866c0326bdaaf58759d4b406
Chris M <cmok97@gmail.com>
2022-05-10 14:26:19 -0700

    wrote tdd tests for PUT, PATCH, POST, DELETE by id  routes

--- test/test_app.js
+++ test/test_app.js
@@ -54,6 +54,7 @@ describe("app.js API", () => {
     // Test POST
     describe("POST new movie entry", () => {
         it("It should create a new movie entry", (done) => {
+            let movieid = 1;
             let movie = new Movie ({
                 username: "Chris",
                 name: "Doctor Strange in the Multiverse of Madness",
@@ -68,12 +69,83 @@ describe("app.js API", () => {
                 .end((err, res) => {
                     res.should.have.status(201);
                     res.body.should.be.a("object");
+                    response.body.should.have.property("id").eq(1);
                     response.body.should.have.property("name").eq("Chris");
                     response.body.should.have.property("username").eq("Doctor Strange in the Multiverse of Madness");
                     response.body.should.have.property("genre").eq("horror");
                     response.body.should.have.property("release_date").eq(05);
                     response.body.should.have.property("status").eq(true);
                 movie.remove();
+                done();
+                });
+        });
+    });
+
+    // Test PUT
+    describe("PUT new movie entry at /tracker/form/:id", () => {
+        it("It should PUT /tracker/form/:id", (done) => {
+            const movieid = 1;
+            let movie = new Movie ({
+                username: "Chris",
+                name: "Doctor Strange in the Multiverse of Madness",
+                genre: "horror",
+                release_date: 05,
+                status: true
+            })
+            movie.save();
+            chai.request(server)
+                .put("/tracker/form/" + movieid)
+                .send(movie)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    response.body.should.have.property("id").eq(1);
+                    response.body.should.have.property("name").eq("Chris");
+                    response.body.should.have.property("username").eq("Doctor Strange in the Multiverse of Madness");
+                    response.body.should.have.property("genre").eq("horror");
+                    response.body.should.have.property("release_date").eq(05);
+                    response.body.should.have.property("status").eq(true);
+                movie.remove();
+                done();
+                });
+        });
+    });
+
+    // Test PATCH
+    describe("PATCH existing movie entry at /tracker/form/:id", () => {
+        it("It should PATCH movie from /tracker/form/:id", (done) => {
+            const movieid = 1;
+            let movie = {
+                name: "Thor 4 More Thor",
+                genre: "Comedy",
+                status: false
+            }
+            chai.request(server)
+                .patch("/tracker/form/" + movieid)
+                .send(movie)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    response.body.should.have.property("id").eq(1);
+                    response.body.should.have.property("name").eq("Chris");
+                    response.body.should.have.property("username").eq("Thor 4 More Thor");
+                    response.body.should.have.property("genre").eq("Comedy");
+                    response.body.should.have.property("release_date").eq(05);
+                    response.body.should.have.property("status").eq(false);
+                movie.remove();
+                done();
+                });
+        });
+    });
+
+    // Test DELETE
+    describe("DELETE existing movie entry at /tracker/form/:id", () => {
+        it("It should DELETE movie from /tracker/form/:id", (done) => {
+            let movieid = 1;
+            chai.request(server)
+                .delete("/tracker/form/" + movieid)
+                .end((err, res) => {
+                    res.should.have.status(200);
                 done();
                 });
         });
