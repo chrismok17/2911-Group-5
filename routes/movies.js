7cca4d0293f10b17422ca3ea625dd067aed7e102
Desmond Ho <desmondh268@gmail.com>
2022-05-11 20:25:47 -0700

    routes for view, and form created

--- routes/movies.js
+++ routes/movies.js
@@ -6,21 +6,37 @@ const router = express.Router();
 // the schema
 const Movie = require("../models/Movie");
 
+// all entries
+async function all_users() {
+  let entries = await Movie.find();
+  return entries;
+}
+
 // handles the login,in the real app, this list of user will be from database
 function handleLogin() {
-  let tempusers = ["tim", "johnny"];
-  let form = document.forms[0];
-  var formElement = form.querySelector('input[name="username"]');
-  for (let i = 0; i < tempusers.length; i++) {
-    if (formElement.value == tempusers[i]) {
-      return confirm(
-        `---${formElement.value}--- already exists, Do you wish to proceed?`
-      );
-    }
-  }
-  return confirm(
-    `---${formElement.value}--- Not found, an account will be made`
-  );
+  return confirm(all_users());
+  // let entries = Movie.find()
+  // let entries = [{ username: "Bobby222" }];
+  // let users = [];
+  // for (let i = 0; i < entries.length; i++) {
+  //   users.push(entries[i]["username"]);
+  // }
+  // let user_set = new Set(users);
+  // let form = document.forms[0];
+  // var formElement = form.querySelector('input[name="username"]');
+  // if (formElement.value == "") {
+  //   return alert("Please provide a username!");
+  // }
+  // user_set.forEach((user) => {
+  //   if (formElement.value == user) {
+  //     return confirm(
+  //       `---${formElement.value}--- already exists, Do you wish to proceed?`
+  //     );
+  //   }
+  // });
+  // return confirm(
+  //   `---${formElement.value}--- Not found, an account will be made`
+  // );
 }
 
 // to handle data
@@ -28,32 +44,38 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 // same as get request for view, shows table of data
-router.post("/view", (req, res) => {
-  let info = {
-    johnny: "Johnny's Credit Card Number: 7772 3234 6746 2124",
-    tim: "Tims dark secret: I like dipping my chocolate in MAYO ",
-  };
+router.post("/view", async (req, res) => {
+  let user_movies = await Movie.find({ username: req.body.username });
   res.send(
-    `<h1>Your Username is :  ${req.body.username} || ${
-      info[req.body.username]
-    }</h1>`
+    `<h1>Your Username is :  ${req.body.username} || 
+     ---${user_movies}</h1>`
   );
 });
 
 // post request to form, add data to database
 router.post("/form", (req, res) => {
-  if (req.body._id == "") {
+  if (req.body._id == undefined) {
     new_movie(req, res);
   } else {
     update_movie(req, res);
   }
 });
 
+// get request for "create new" button
+router.get("/form/:username", (req, res) => {
+  res.send(`Create page ${req.params.username}`);
+});
+
+// get request for edit for edit button
+router.get("/form/edit", (req, res) => {
+  res.send("Edit page");
+});
+
 // update movie function, updates existing movie entry
-function update_movie(req, res) {
+async function update_movie(req, res) {
   console.log(req.body, "Update");
-  Movie.findOneAndUpdate(
-    { _id: req.body.id },
+  await Movie.findOneAndUpdate(
+    { _id: req.body._id },
     {
       username: req.body.username,
       name: req.body.name,
@@ -62,11 +84,16 @@ function update_movie(req, res) {
       status: req.body.status,
     }
   );
+  let updated_movie = await Movie.findById({ _id: req.body._id });
+  res.json(updated_movie);
 }
 
 // insert movie function, adds a new entry to the database
-function new_movie(req, res) {
+async function new_movie(req, res) {
   console.log(req.body, "New");
+  let movie_entry = new Movie(req.body);
+  let send_entry = await movie_entry.save();
+  res.status(201).json(send_entry);
 }
 
 module.exports = router;
