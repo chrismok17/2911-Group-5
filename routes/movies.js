2075c1f2111cbaa0ccfa1e87542a26f78086567d
Desmond Ho <desmondh268@gmail.com>
2022-05-12 16:03:09 -0700

    Ammended the routes, buttons now connect with mongo, delete not working yet

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = require("../app");
+const path = require("path");
+
 // express router
 const router = express.Router();
 
@@ -47,7 +49,7 @@ router.post("/view", async (req, res) => {
 
 // post request to form, add data to database
 router.post("/form", (req, res) => {
-  if (req.body._id == undefined) {
+  if (req.body.id == undefined || req.body.id == "" || req.body.id == "nah") {
     new_movie(req, res);
   } else {
     update_movie(req, res);
@@ -55,42 +57,78 @@ router.post("/form", (req, res) => {
 });
 
 // get request for "create new" button
-router.get("/form/:username", (req, res) => {
+router.get("/form/:username/", (req, res) => {
   // sends in username
-  res.send(`Create page ${req.params.username}`);
+  res.render("create_edit", {
+    action: "New",
+    username: req.params.username,
+    id: "",
+    name: "",
+    genre: "",
+    release_date: "",
+    status: "",
+  });
 });
 
 // get request for edit for edit button
-router.get("/form/edit", (req, res) => {
-  res.send("Edit page");
+router.post("/form/edit", async (req, res) => {
+  let edit_instance = await Movie.find({ _id: req.body.id });
+  // res.send(req.body.username);
+  res.render("create_edit", {
+    action: "Edit",
+    username: req.body.username,
+    name: req.body.name,
+    genre: req.body.genre,
+    status: req.body.status,
+    release_date: req.body.release_date,
+    id: req.body.id,
+    movie: edit_instance,
+  });
 });
 
 // update movie function, updates existing movie entry
 async function update_movie(req, res) {
-  console.log(req.body, "Update");
-  await Movie.findOneAndUpdate(
-    { _id: req.body._id },
+  let update_instance = await Movie.findOneAndUpdate(
+    { _id: req.body.id },
     {
       username: req.body.username,
       name: req.body.name,
       genre: req.body.genre,
       release_date: req.body.release_date,
-      status: req.body.status,
+      status: req.body.status === "true",
     }
   );
   // let updated_movie = await Movie.findById({ _id: req.body._id });
   let movies = await Movie.find({ username: req.body.username });
-  res.json(movies);
+  // res.json(movies);
+  console.log(movies);
+  res.render("view", { username: req.body.username, movies: movies });
 }
 
 // insert movie function, adds a new entry to the database
 async function new_movie(req, res) {
   console.log(req.body, "New");
-  let movie_entry = new Movie(req.body);
+  let movie_entry = new Movie({
+    username: req.body.username,
+    name: req.body.name,
+    genre: req.body.genre,
+    release_date: req.body.release_date,
+    status: req.body.status,
+  });
   let send_entry = await movie_entry.save();
   // res.status(201).json(send_entry);
   let movies = await Movie.find({ username: req.body.username });
-  res.json(movies);
+  res.render("view", { movies: movies, username: req.body.username });
+
+  // {
+  //   action: "Create",
+  //   username: req.body.username,
+  //   id: "",
+  //   name: "",
+  //   genre: "",
+  //   release_date: "",
+  //   status: "",
+  // }
 }
 
 // delete by ID route
