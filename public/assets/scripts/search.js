1dc31d8ea58fb5a2fa9e1dc45d3abc28f4e5e86d
Chris M <cmok97@gmail.com>
2022-05-16 14:59:09 -0700

    added ability to search by each column

--- public/assets/scripts/search.js
+++ public/assets/scripts/search.js
@@ -1,15 +1,17 @@
 function search() {
-    let input, filter, table, tablerow, tabledata, i, txtValue;
+    let input, filter, table, tablerow, i;
     input = document.getElementById("searchbar");
     filter = input.value.toUpperCase();
     table = document.getElementById("viewtable");
     tablerow = table.getElementsByTagName("tr");
 
     for (i = 0; i < tablerow.length; i++) {
-        tabledata = tablerow[i].getElementsByTagName("td")[0];
-        if (tabledata) {
-            txtValue = tabledata.textContent || tabledata.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        let movie_name = tablerow[i].getElementsByTagName("td")[0];
+        let movie_genre = tablerow[i].getElementsByTagName("td")[1]
+        let movie_date = tablerow[i].getElementsByTagName("td")[2]
+        let movie_status = tablerow[i].getElementsByTagName("td")[3]
+        if (movie_name) {
+            if (movie_name.textContent.toUpperCase().indexOf(filter) > -1 || movie_genre.textContent.toUpperCase().indexOf(filter) > -1 || movie_date.textContent.toUpperCase().indexOf(filter) > -1 || movie_status.textContent.toUpperCase().indexOf(filter) > -1) {
                 tablerow[i].style.display= "";
             }
             else {
