6a2859cf52bee0849a57eadd0d026929c2fdd73f
Desmond Ho <desmondh268@gmail.com>
2022-05-19 15:42:57 -0700

    changed schema

--- models/Movie.js
+++ models/Movie.js
@@ -4,23 +4,18 @@ const mongoose = require("mongoose");
 
 const MovieSchema = new mongoose.Schema({
   username: {
-    required: true,
     type: String,
   },
   name: {
-    required: true,
     type: String,
   },
   genre: {
-    required: true,
     type: String,
   },
   release_date: {
-    required: true,
     type: Number,
   },
   status: {
-    required: true,
     type: Boolean,
   },
 });
